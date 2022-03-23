0c1030508969527572e2a54f3e7dd914ed6eff03
kainara <kaiappelt53@gmail.com>
2022-03-23 19:59:03 -0300

    Ajustes de doc

--- ormconfig.js
+++ ormconfig.js
@@ -2,16 +2,22 @@ require('dotenv/config');
 
 module.exports = {
     type: process.env.DB_TYPE,
-    url: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 
     entities: [process.env.ENTITIES],
     migrations: [process.env.MIGRATIONS],
     cli: {
         migrationsDir: process.env.MIGRATIONS_DIR,
     },
-    extra: {
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    },
+
+    // Utilizar somente com banco de dados em nuvem
+    // extra: {
+    //     ssl: {
+    //         rejectUnauthorized: false,
+    //     },
+    // },
 };
